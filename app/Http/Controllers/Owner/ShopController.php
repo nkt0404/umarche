f7fd94793001bcf3930b62acd4d6480730f3af02
nkt0404 <nktorun7001@gmail.com>
2022-12-18 16:33:55 +0900

    画像　フォームリクエスト

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
+use Illuminate\Support\Facades\Storage;
+use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 
 class ShopController extends Controller
 {
@@ -37,6 +40,7 @@ class ShopController extends Controller
 
     public function index()
     {
+        //phpinfo();
         //$ownerId = Auth::id();
         $shops = Shop::where('owner_id', Auth::id())->get();
 
@@ -45,9 +49,24 @@ class ShopController extends Controller
 
     public function edit($id)
     {
+        $shop = Shop::findOrFail($id);
+        return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
+        //一時保存
+        $imageFile = $request->image;
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            //リサイズなしの場合
+            //Storage::putFile('public/shops', $imageFile);
+            $fileName = uniqid(rand() . '_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName . '.' . $extension;
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
+        }
+
+        return redirect()->route('owner.shops.index');
     }
 }
