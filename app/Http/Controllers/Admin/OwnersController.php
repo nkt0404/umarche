a0a3ba3aa48c7fb408ecc003dda2ef618826e38b
nkt0404 <nktorun7001@gmail.com>
2022-12-18 11:45:36 +0900

    shopCreateDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -8,6 +8,9 @@ use App\Models\Owner; // Elquent
 use Illuminate\Support\Facades\DB; //QueryBuilder クエリビルダ
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+use App\Models\Shop;
 
 class OwnersController extends Controller
 {
@@ -71,11 +74,29 @@ class OwnersController extends Controller
             'password' => 'required|string|confirmed|min:8',
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try {
+            DB::transaction(
+                function () use ($request) {
+                    $owner = Owner::create([
+                        'name' => $request->name,
+                        'email' => $request->email,
+                        'password' => Hash::make($request->password),
+                    ]);
+                    Shop::create([
+                        'owner_id' => $owner->id,
+                        'name' => '店名を入力してください',
+                        'information' => '',
+                        'filename' => '',
+                        'is_selling' => true,
+                    ]);
+                },
+                2
+            );
+        } catch (Throwable $e) {
+            Log::error($e);
+            throw $e;
+        }
+
 
         return redirect()
             ->route('admin.owners.index')
