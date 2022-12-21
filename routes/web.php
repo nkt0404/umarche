ecaf2f3001f424e231a6c263153f527bfced27ab
nkt0404 <nktorun7001@gmail.com>
2022-12-22 05:29:43 +0900

    cart add

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
 use App\Http\Controllers\User\ItemController;
+use App\Http\Controllers\User\CartController;
 
 
 /*
@@ -26,9 +27,9 @@ Route::middleware('auth:users')->group(function () {
     Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('user.dashboard');
-// })->middleware(['auth:users'])->name('dashboard');
+Route::prefix('cart')->middleware('auth:users')->group(function () {
+    Route::post('add', [CartController::class, 'add'])->name('cart.add');
+});
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
