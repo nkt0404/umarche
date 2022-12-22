b2e2bd9499047e82ee89b7a4cec5322d347745e4
nkt0404 <nktorun7001@gmail.com>
2022-12-23 05:16:41 +0900

    stripeキャンセル処理

--- resources/views/user/checkout.blade.php
+++ resources/views/user/checkout.blade.php
@@ -7,6 +7,8 @@
     window.onload = function() {
         stripe.redirectToCheckout({
             sessionId: '{{ $session->id }}'
-        }).then(function(result) {});
+        }).then(function(result) {
+            window.location.href = '{{ route('user.cart.cancel') }}'
+        });
     }
 </script>
