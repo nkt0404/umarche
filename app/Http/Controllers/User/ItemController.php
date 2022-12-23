65151e657bfeec9534833532874725e56ef83ba0
nkt0404 <nktorun7001@gmail.com>
2022-12-23 22:55:47 +0900

    メールキュー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
+use App\Jobs\SendThanksMail;
 
 class ItemController extends Controller
 {
@@ -32,7 +33,11 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
-        Mail::to('test@example.com')->send(new TestMail());
+        //同期的に送信
+        // Mail::to('test@example.com')->send(new TestMail());
+        //非同期に送信
+        SendThanksMail::dispatch();
+
         $categories = PrimaryCategory::with('secondary')->get();
 
         $products = Product::availableItems()
