d2a21a2e3d10a53eed7217e76d65484df228a76a
nkt0404 <nktorun7001@gmail.com>
2022-12-23 15:37:59 +0900

    表示順

--- app/Constants/Common.php
+++ app/Constants/Common.php
@@ -11,4 +11,18 @@ class Common
         'add' => self::PRODUCT_ADD,
         'reduce' => self::PRODUCT_REDUCE
     ];
+
+    const ORDER_RECOMMEND = '0';
+    const ORDER_HIGER = '1';
+    const ORDER_LOWER = '2';
+    const ORDER_LATER = '3';
+    const ORDER_OLDER = '4';
+
+    const SORT_ORDER = [
+        'recommend' => self::ORDER_RECOMMEND,
+        'higherPrice' => self::ORDER_HIGER,
+        'lowerPrice' => self::ORDER_LOWER,
+        'later' => self::ORDER_LATER,
+        'older' => self::ORDER_OLDER
+    ];
 }
